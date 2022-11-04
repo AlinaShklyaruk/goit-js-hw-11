9fa02c9ea60415dd70e74fe9386a5cb65ce5045c
AlinaShklyaruk <i.charge92@gmail.com>
2022-11-04 17:02:41 +0200

    refs created

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.5.min.css";
 
 const axios = require('axios');
+
+const inputEl = document.querySelector('#search-form');
+const searchBtnEl = document.querySelector('button[submit]');
+const galleryEl = document.querySelector('div.gallery');
+
+inputEl.addEventListener('input', onInput);
+function onInput(event) {
+    const searchQuery = event.target.searchQuery.value;
+    console.log(searchQuery);
+}
