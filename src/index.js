0113b6fcd63bd1761c3ae67b1100f57a365f2f4a
AlinaShklyaruk <i.charge92@gmail.com>
2022-11-06 14:56:08 +0200

    simplelightbox added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.5.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const axios = require('axios');
 
