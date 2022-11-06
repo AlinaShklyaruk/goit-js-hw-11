ac396485601aa3dd421ceb083191efe6d4fce638
AlinaShklyaruk <i.charge92@gmail.com>
2022-11-06 22:56:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,18 @@ const inputEl = document.querySelector('#search-form');
 const searchBtnEl = document.querySelector('button[submit]');
 const galleryEl = document.querySelector('div.gallery');
 
-inputEl.addEventListener('input', onInput);
+inputEl.addEventListener('submit', onInput);
 function onInput(event) {
-    const searchQuery = event.target.searchQuery.value;
-    console.log(searchQuery);
+    const searchQuery = event.target.elements.searchQuery.value;
+    getUser(searchQuery).then(response => response.json());
+    console.log(getUser(searchQuery));
+    console.log(response);
+}
+async function getUser() {
+  try {
+    const response = await axios.get(`https://pixabay.com/api/?key=31136812-0f1a6a81def38a9fb9e0c181f&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
