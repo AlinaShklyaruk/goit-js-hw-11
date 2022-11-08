a4ca85f578010da4161cb86763462857437b4799
AlinaShklyaruk <i.charge92@gmail.com>
2022-11-08 12:01:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,22 +6,8 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const axios = require('axios');
 
-const inputEl = document.querySelector('#search-form');
+const formEl = document.querySelector('#search-form');
+const inputEl = document.querySelector('input[name="searchQuery"]')
 const searchBtnEl = document.querySelector('button[submit]');
 const galleryEl = document.querySelector('div.gallery');
 
-inputEl.addEventListener('submit', onInput);
-function onInput(event) {
-    const searchQuery = event.target.elements.searchQuery.value;
-    getUser(searchQuery).then(response => response.json());
-    console.log(getUser(searchQuery));
-    console.log(response);
-}
-async function getUser() {
-  try {
-    const response = await axios.get(`https://pixabay.com/api/?key=31136812-0f1a6a81def38a9fb9e0c181f&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
